6edeb798b56b51388b6f7649c6600bf9473b26c8
Jonhatan <jonyvilla44@gmail.com>
2025-02-27 15:55:18 -0600

    Modificando, rutas(imagenes), cv y versiones

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -4,7 +4,7 @@ export const projects = [
         name: "JSON Placeholder API",
         date: "08/28/2024",
         description: "Consuming almost all data from the api and show it in my app. Using React, CSS, HTML, React-hook-form, TanStack Query, Material UI, axios & more.",
-        image: "/images/project-1.png",
+        image: "/src/assets/images/project-1.png",
         link: "https://json-placeholder-app.netlify.app/",
         linkGit: "https://github.com/ProgramadorXP/JSON-Placeholder-API",
         techs: {
@@ -20,7 +20,7 @@ export const projects = [
         name: "The CocktailDB",
         date: "05/15/2023",
         description: "Query the 'TheCocktailDB' API to interact with its data and display it in the app. Technologies used: React, react router dom, tailwind css, zod, zustand, axios, heroicons, headlessui with performance improvements using react's lazy and suspense.",
-        image: "/images/project-2.png",
+        image: "/src/assets/images/project-2.png",
         link: "https://phenomenal-otter-9e4904.netlify.app/",
         linkGit: "https://github.com/ProgramadorXP/bebidas-react-typescript",
         techs: {
@@ -39,7 +39,7 @@ export const projects = [
         name: "Patient Control",
         date: "12/08/2023",
         description: "This is a patient control application using React, TypeScript, Tailwind CSS, zustand, uuidv4, react-toastify, react-hook-form",
-        image: "/images/project-3.png",
+        image: "/src/assets/images/project-3.png",
         link: "https://pacientes-zustand.netlify.app/",
         linkGit: "https://github.com/ProgramadorXP/pacientes-zustand",
         techs: {
@@ -56,7 +56,7 @@ export const projects = [
         name: "GuitarLA",
         date: "06/01/2023",
         description: "Simulating the purchase of guitars to add them to the cart. Technologies used: React, TypeScript",
-        image: "/images/project-4.png",
+        image: "/src/assets/images/project-4.png",
         link: "https://guitarlatsreducer.netlify.app/",
         linkGit: "https://github.com/ProgramadorXP/guitarLA-TS",
         techs: {
